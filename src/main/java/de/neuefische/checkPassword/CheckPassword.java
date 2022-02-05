b081f033d24fb853bebdfe9fb69c100ec5127da5
Steve Dölgner <steve.doelgner@gmail.com>
2022-02-05 10:24:24 +0100

    recursive added

--- src/main/java/de/neuefische/checkPassword/CheckPassword.java
+++ src/main/java/de/neuefische/checkPassword/CheckPassword.java
@@ -1,11 +1,13 @@
 package de.neuefische.checkPassword;
 
+import java.util.Arrays;
+
 public class CheckPassword {
 
     public static void main(String[] args) {
 
-        String[] passwords = {"Ab123456789", "A123456789", "AA123456789"};
-        System.out.println(validate( passwords));
+        String[] passwords = {"Ab123456789", "A123a", "AA123456789"};
+        System.out.println(validate(passwords));
     }
 
 
@@ -14,27 +16,29 @@ public class CheckPassword {
         boolean validPassword = false;
         String output ="";
 
-        for (int i = 0; i < choosedPasswords.length; i++) {
-
-            boolean passwordLength = false;
-
-            if (choosedPasswords[i].length() >= 11) {
-
-                passwordLength = true;
-                boolean checkNumbers = choosedPasswords[i].matches(".*[0-9].*");
-                boolean checkUpper = choosedPasswords[i].matches(".*[A-Z].*");
-                boolean checkLower = choosedPasswords[i].matches(".*[a-z].*");
-
-                if (checkNumbers && checkUpper && checkLower) {
-                    validPassword = true;
-                    output = "Dein Passwort ist sicher!";
-                } else {
-                    output = "Dein Passwort muss Großbuchstaben, Kleinbuchstaben und Ziffern enthalten!";
-                }
-            } else {
-                output = "Dein Passwort ist zu kurz!";
-            }
+        if (choosedPasswords.length == 0) {
+            return output;
         }
-        return output;
+
+        boolean passwordLength = false;
+
+        if (choosedPasswords[choosedPasswords.length - 1].length() >= 11) {
+
+            passwordLength = true;
+            boolean checkNumbers = choosedPasswords[choosedPasswords.length - 1].matches(".*[0-9].*");
+            boolean checkUpper = choosedPasswords[choosedPasswords.length - 1].matches(".*[A-Z].*");
+            boolean checkLower = choosedPasswords[choosedPasswords.length - 1].matches(".*[a-z].*");
+
+            if (checkNumbers && checkUpper && checkLower) {
+                validPassword = true;
+                output = "Dein Passwort ist sicher!";
+            } else {
+                output = "Dein Passwort muss Großbuchstaben, Kleinbuchstaben und Ziffern enthalten!";
+            }
+        } else {
+            output = "Dein Passwort ist zu kurz!";
+        }
+
+        return validate(Arrays.copyOf(choosedPasswords, choosedPasswords.length -1)) + output;
     }
 }
