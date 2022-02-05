b75149f30ba5638d9d53f9db78523d129405d0d4
Steve Dölgner <steve.doelgner@gmail.com>
2022-02-05 08:03:00 +0100

    loop added

--- src/main/java/de/neuefische/checkPassword/CheckPassword.java
+++ src/main/java/de/neuefische/checkPassword/CheckPassword.java
@@ -3,39 +3,47 @@ package de.neuefische.checkPassword;
 public class CheckPassword {
 
     public static void main(String[] args) {
-        System.out.println(validate( "Ab1123456789"));
+
+        String[] passwords = {"Ab123456789", "A123456789", "Ab23456789"};
+        System.out.println(validate( passwords));
     }
 
 
-    public static boolean validate(String choosedPassword) {
+    public static boolean validate(String[] choosedPasswords) {
 
         boolean validPassword = false;
-        boolean passwordLength = false;
 
-        if(choosedPassword.length() >= 11) {
-            passwordLength = true;
-        } else {
-            System.out.println("Dein Passwort ist zu kurz!");
-        }
+        for (int i = 0; i < choosedPasswords.length; i++) {
 
-        boolean checkNumbers = choosedPassword.matches(".*[0-9].*");
-        if(!checkNumbers) {
-            System.out.println("Dein Passwort muss Zahlen enthalten!");
-        }
+            boolean passwordLength = false;
 
-        boolean checkUpper = choosedPassword.matches(".*[A-Z].*");
-        if (!checkUpper) {
-            System.out.println("Dein Passwort muss Großbuchstaben enthalten!");
-        }
+            if (choosedPasswords[i].length() >= 11) {
 
-        boolean checkLower = choosedPassword.matches(".*[a-z].*");
-        if (!checkLower) {
-            System.out.println("Dein Passwort muss Kleinbuchstaben enthalten!");
-        }
+                passwordLength = true;
+            } else {
+                System.out.println("Dein Passwort ist zu kurz!");
+            }
 
-        if (passwordLength && checkNumbers && checkUpper && checkLower) {
-            validPassword = true;
-            System.out.println("Dein Passwort ist sicher!");
+            boolean checkNumbers = choosedPasswords[i].matches(".*[0-9].*");
+            if (!checkNumbers) {
+                System.out.println("Dein Passwort muss Zahlen enthalten!");
+            }
+
+            boolean checkUpper = choosedPasswords[i].matches(".*[A-Z].*");
+            if (!checkUpper) {
+                System.out.println("Dein Passwort muss Großbuchstaben enthalten!");
+            }
+
+            boolean checkLower = choosedPasswords[i].matches(".*[a-z].*");
+            if (!checkLower) {
+                System.out.println("Dein Passwort muss Kleinbuchstaben enthalten!");
+            }
+
+            if (passwordLength && checkNumbers && checkUpper && checkLower) {
+                validPassword = true;
+                System.out.println("Dein Passwort ist sicher!");
+            }
+
         }
 
         return validPassword;
